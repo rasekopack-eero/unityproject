7dcadfcb531d10b904a38ddcc194d2385e3adafb
itsYesseh <itsyesseh@gmail.com>
2020-11-22 23:00:56 +0200

    Create RaycastScript.cs

---
+++ gaem/Assets/Scripts/RaycastScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.InverseTransformDirection(Vector3.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit))
+        {
+            if (hit.transform.gameObject.tag == "enemy")
+            {
+                Debug.Log("enemy hit");
+            }
+        }
+    }
+}
