e666ca54c9e38a28e21704808f09705f34bbb634
rasekopack-eero <kapteeniluttinen@gmail.com>
2020-11-23 23:59:59 +0200

    ai pathfind

---
+++ gaem/Assets/Scripts/AIScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIScript : MonoBehaviour
+{
+    public Transform destination;
+    NavMeshAgent navMA;
+    
+    void Start()
+    {
+        navMA = GetComponent<NavMeshAgent>();
+        SetDestination();
+    }
+    void Update()
+    {
+        
+    }
+    void SetDestination()
+    {
+        Vector3 target = destination.transform.position;
+        navMA.SetDestination(target);
+    }
+}
